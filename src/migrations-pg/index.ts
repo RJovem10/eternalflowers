926e73368f5e2d447de9ad33f302531ce7afb7af
Jovem <casaferreira1950@gmail.com>
2026-08-01 12:11:32 +0100

    feat(payload): ISSUE-016 adiciona migration PostgreSQL para categorias

--- src/migrations-pg/index.ts
+++ src/migrations-pg/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260801_094419_flowers_story_localized_pg from './20260801_094419_flowers_story_localized_pg';
+import * as migration_20260801_105830_categories_localized_pg from './20260801_105830_categories_localized_pg';
 
 export const migrations = [
   {
     up: migration_20260801_094419_flowers_story_localized_pg.up,
     down: migration_20260801_094419_flowers_story_localized_pg.down,
     name: '20260801_094419_flowers_story_localized_pg'
+  },
+  {
+    up: migration_20260801_105830_categories_localized_pg.up,
+    down: migration_20260801_105830_categories_localized_pg.down,
+    name: '20260801_105830_categories_localized_pg'
   },
 ];
